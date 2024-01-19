e2b39de87b701c10dd09e9fdd6434bb3583e85fb
LawnOfTheDead <145202101+LawnOfTheDead@users.noreply.github.com>
2024-01-19 15:54:05 +0000

    Info To Teams  Update

--- settings.js
+++ settings.js
@@ -3,6 +3,7 @@ function setViewMode()
     var viewMode = localStorage.getItem('view-mode');
     var body = document.getElementsByTagName('body')[0];
 
+    
     body.classList.remove('dark-mode');
     body.classList.remove('rainbow-mode');
 
