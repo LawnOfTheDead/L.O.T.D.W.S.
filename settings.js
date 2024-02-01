9e8a7061d51b92364c067947e74dba9921bd8088
LawnOfTheDead <145202101+LawnOfTheDead@users.noreply.github.com>
2024-02-01 12:59:24 +0000

    Current exact website version: v06.22.63 (Last Update Tue 30th Jan 2024 )

--- settings.js
+++ settings.js
@@ -1,16 +1,14 @@
-function setViewMode()
-  {
-    var viewMode = localStorage.getItem('view-mode');
-    var body = document.getElementsByTagName('body')[0];
+function setViewMode() {
+  var viewMode = localStorage.getItem('view-mode');
+  var body = document.getElementsByTagName('body')[0];
 
-    
-    body.classList.remove('dark-mode');
-    body.classList.remove('rainbow-mode');
 
-    if (viewMode)
-    {
-      body.classList.add(viewMode);
-    }
+  body.classList.remove('dark-mode');
+  body.classList.remove('rainbow-mode');
+
+  if (viewMode) {
+    body.classList.add(viewMode);
   }
+}
 
 setViewMode();
